b159bbf904355be74f794303a2f7d1527fca7bd9
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-03-24 14:43:31 +0600

    Project Setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './App.css'
 const App = () => {
     return (
         <div>
-            <h1>Hey I am the app</h1>
+            <h1 className='underline text-2xl'>Hey I am the app</h1>
         </div>
     );
 };
